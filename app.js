14529fd7224c374e98b78dde7be74bf2dfc890da
naibahmed <naibahmed96@gmail.com>
2020-09-06 10:35:37 +0300

    Remove single list from local storage

--- app.js
+++ app.js
@@ -95,9 +95,31 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
     if (confirm('Are you sure?')) {
       e.target.parentElement.parentElement.remove();
+
+      removeTaskFromLocalStorage(
+        e.target.parentElement.parentElement);
     }
   }
   
+}
+
+// Remove items from localStorage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task, index) => {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // ClearTask function
@@ -108,6 +130,8 @@ function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+ 
 }
 
 // Store task
